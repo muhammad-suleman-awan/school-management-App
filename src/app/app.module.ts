1968a00912a6c7fb8e81cc6238751d180605b594
muhammad-suleman-awan <mmsulemanawan@gmail.com>
2023-03-27 19:41:07 +0500

    Work on add Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+import {MatButtonModule} from '@angular/material/button';
+
+
 import {MatIconModule} from '@angular/material/icon';
 import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 
@@ -24,6 +30,9 @@ import { NavigationbarComponent } from './navigationbar/navigationbar.component'
     BrowserAnimationsModule,
 
 
+    MatButtonModule,
+    MatListModule,
+    MatSidenavModule,
     MatIconModule,
     MatToolbarModule,
 
