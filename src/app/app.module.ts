853b121a9e94d25d167190faa0e331df6e59816a
muhammad-suleman-awan <mmsulemanawan@gmail.com>
2023-03-27 16:49:18 +0500

    Adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+
+    MatIconModule,
+    MatToolbarModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigationbar/navigationbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigationbar',
+  templateUrl: './navigationbar.component.html',
+  styleUrls: ['./navigationbar.component.scss']
+})
+export class NavigationbarComponent {
+
+}
