51e5b2115879faa7563cd4cc86a267e30e6e6f6c
muhammad-suleman-awan <mmsulemanawan@gmail.com>
2023-03-28 10:05:40 +0500

    ok

--- src/app/registrationform/registrationform.component.ts
+++ src/app/registrationform/registrationform.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './registrationform.component.html',
   styleUrls: ['./registrationform.component.scss']
 })
-export class RegistrationformComponent {
+export class RegistrationformComponent  {
+
+  submit(){
+    console.log("Form submitted")
+  }
+
 
 }
+
